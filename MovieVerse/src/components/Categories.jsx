fbea7b5d2e06461e20bbacf26a3112e409406812
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-06 22:31:36 +0700

    .

--- MovieVerse/src/components/Categories.jsx
+++ MovieVerse/src/components/Categories.jsx
@@ -1,7 +1,22 @@
 export default function Categories() {
     return(
         <div>
-            categories
+            <div className="relative">
+                <button>
+                    Genres
+                    {/* buat 2 icon if isOpened true panah kebawah
+                    if isOpened false maka panah kearah kanan (nilai default) */}
+                </button>
+                {/* logic buat munculin menunya (dibuat absolut ketika isOpened true) */}
+            </div>
+            <div className="relative">
+                <button>
+                    Released
+                    {/* buat 2 icon if isOpened true panah kebawah
+                    if isOpened false maka panah kearah kanan (nilai default) */}
+                </button>
+                {/* logic buat munculin menunya (dibuat absolut ketika isOpened true) */}
+            </div>
         </div>
     )
 };
