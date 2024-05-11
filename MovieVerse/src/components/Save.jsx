36a71d1962414f75e87d5c81970aa40a83a45d73
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-11 17:12:35 +0700

    .

--- MovieVerse/src/components/Save.jsx
+++ MovieVerse/src/components/Save.jsx
@@ -7,19 +7,22 @@ import axios from 'axios'
 export default function Save({details}) {
     const [isSaved, setIsSaved] = useState(false)
     const URL = `${import.meta.env.VITE_REACT_API_MOCK}/saved/`
-    // test
     const [id, setId] = useState('')
+    const [status, setStatus] = useState(save)
     useEffect(()=>{
         getLikedMovies()
     },[])
     async function getLikedMovies(){
         try{
             const response = await axios.get(URL)
-            response.data.forEach(item => {
-                if(item.imdbID === details.imdbID){
-                    setId(item.id)
-                }
-            })
+            if(response.data.length > 0){
+                response.data.forEach(item => {
+                    if(item.imdbID === details.imdbID){
+                        setId(item.id)
+                        setStatus(item.status)
+                    }
+                })
+            }
         }catch(err){
             console.log(err)
         }
@@ -36,26 +39,31 @@ export default function Save({details}) {
                 confirmButtonText: 'OK'
             })
             try{
-                await axios.post(URL,{...details})
+                await axios.post(URL,{...details, status: saved})
             }catch(err){
                 console.log(err)
+            }finally{
+                location.reload()
             }
         }else{
+            Swal.fire({
+                title: 'Removed from saved!',
+                text: 'Press OK to continue',
+                icon: 'error',
+                confirmButtonText: 'OK'
+            })
             try{
                 await axios.delete(URL+id)
-                Swal.fire({
-                    title: 'Removed from saved!',
-                    text: 'Press OK to continue',
-                    icon: 'error',
-                    confirmButtonText: 'OK'
-                })
+                setStatus(save)
             }catch(err){
                 console.log(err)
+            }finally{
+                location.reload()
             }
         }
     }
     return (
-        <img src={isSaved? saved : save} alt="save" 
+        <img src={status} alt="save" 
         className='w-[20px] h-[20px]' onClick={handleClick}/>
     )
 };
