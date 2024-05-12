cfd03daea01d736383eb0e6ccf85330c48374054
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-12 16:35:15 +0700

    feat: logout

--- MovieVerse/src/components/Navbar.jsx
+++ MovieVerse/src/components/Navbar.jsx
@@ -1,13 +1,16 @@
+import { useState } from 'react'
 import save from '../assets/save-icon.png'
 import profile from '../assets/user-icon.png'
 import { Link, useNavigate } from 'react-router-dom'
 
 export default function Navbar() {
     const navigate = useNavigate()
+    const [isHidden, setIsHidden] = useState(true)
     function handleLogout(){
         sessionStorage.removeItem('isLogin')
         navigate('/login')
     }
+    console.log(isHidden)
     return(
         <nav className='flex justify-between px-[50px] pt-[17px] max-lg:flex-col
         max-lg:items-center max-lg:gap-[20px] max-lg:pb-[15px] sticky top-0 bg-[#020510]
@@ -29,13 +32,15 @@ export default function Navbar() {
             <div className='flex gap-[38px] items-center'>
                 <Link to={'/saved'}>
                     <img src={save} alt="save" className='w-[20px] h-[20px] 
-                    transition-[150ms] hover:scale-[1.1] hover:opacity-[0.6] cursor-pointer'/>
+                    transition-[150ms] hover:scale-[1.1] hover:opacity-[0.6] cursor-pointer'
+                    />
                 </Link>
                 <div className='relative'>
                     <img src={profile} alt="profile"  className='w-[32px] h-[32px] 
-                    transition-[150ms] hover:scale-[1.03] hover:opacity-[0.6] cursor-pointer'/>
-                    <button className='absolute right-0 top-[34px] bg-[#D40509] py-1 px-2 rounded-sm
-                    hover:opacity-[0.8]' onClick={handleLogout}>Logout</button>
+                    transition-[150ms] hover:scale-[1.03] hover:opacity-[0.6] cursor-pointer'
+                    onClick={() => setIsHidden(!isHidden)}/>
+                    <button className={`${isHidden? "hidden" : "absolute"} right-0 top-[38px] bg-[#D40509] py-1 px-2 rounded-sm
+                    hover:opacity-[0.8] transition-[150ms]`} onClick={handleLogout}>Logout</button>
                 </div>
             </div>
         </nav>
