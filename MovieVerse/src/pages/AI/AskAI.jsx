2bbe54621fb29975066a59499c8433a229bf0f21
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-17 20:14:09 +0700

    feat: added responsive

--- MovieVerse/src/pages/AI/AskAI.jsx
+++ MovieVerse/src/pages/AI/AskAI.jsx
@@ -63,11 +63,15 @@ export default function AskAI() {
     return (
         <div className="bg-[#020510] text-[#FFFFFF]">
             <Navbar />
-            <div className="flex justify-center items-start gap-[30px] py-6 mb-[150px] min-h-[70vh]">
+            <div className="flex justify-center items-start gap-[30px] py-6 mb-[150px] min-h-[70vh]
+            max-md:flex-col-reverse max-md:items-center max-md:min-h-[25vh]">
                 <div className="border-2 border-white">
                     <div className={`min-h-[600px] min-w-[700px] max-w-[700px] bg-blue-950 
                     max-h-[600px] overflow-y-auto ${loading? 'flex justify-center items-center': false}
-                    max-2xl:min-w-[600px] max-2xl:max-w-[600px] max-xl:min-w-[500px] max-xl:max-w-[500px]`}>
+                    max-2xl:min-w-[600px] max-2xl:max-w-[600px] max-xl:min-w-[500px] max-xl:max-w-[500px]
+                    max-2xl:min-h-[550px] max-2xl:max-h-[550px] max-xl:min-h-[450px] max-xl:max-h-[450px]
+                    max-sm:min-h-[250px] max-sm:max-h-[250px] max-sm:min-w-[350px] max-sm:max-w-[350px]
+                    `}>
                         {loading ? 
                         <span className="loader"></span> 
                         : 
@@ -78,7 +82,8 @@ export default function AskAI() {
                         </div>}
                     </div>
                     <div className="flex flex-col min-w-[700px] max-w-[700px]
-                    max-2xl:min-w-[600px] max-2xl:max-w-[600px] max-xl:min-w-[500px] max-xl:max-w-[500px]">
+                    max-2xl:min-w-[600px] max-2xl:max-w-[600px] max-xl:min-w-[500px] max-xl:max-w-[500px]
+                    max-sm:min-w-[350px] max-sm:max-w-[350px]">
                             <textarea type='text'
                                 value={prompt}
                                 onChange={(e) => setPrompt(e.target.value)}
@@ -88,14 +93,16 @@ export default function AskAI() {
                             ></textarea>
                             <button onClick={runChat} disabled={loading || prompt.length === 0}
                                 className="bg-blue-900 w-full h-[40px] hover:opacity-[0.8]
-                                transition-[150ms] cursor-pointer">
+                                transition-[150ms] cursor-pointer
+                                ">
                                 {loading ? "Generating..." : "Generate"}
                             </button>
                     </div>
                 </div>
-                <div className="flex flex-col gap-[15px] max-xl:max-w-[300px]">
+                <div className="flex flex-col gap-[15px] max-xl:max-w-[300px]
+                max-lg:max-w-[200px] max-md:hidden">
                     <h1 className="text-[28px] 
-                    ">Templates Question:</h1>
+                    max-lg:text-[18px] max-md:hidden">Templates Question:</h1>
                     <p className="border-2 border-white p-3 hover:bg-blue-900
                     transition-[150px] cursor-pointer" onClick={()=>setPrompt(templates.questionOne)}
                     >{templates.questionOne}</p>
