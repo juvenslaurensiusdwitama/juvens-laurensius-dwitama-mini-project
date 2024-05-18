99526f70ad52d6cd5340137217352583a84e87b8
Juvens Laurensius Dwitama <98952213+juvenslaurensiusdwitama@users.noreply.github.com>
2024-05-18 21:59:33 +0700

    feat: add responsiveness

--- MovieVerse/src/pages/Home/Header.jsx
+++ MovieVerse/src/pages/Home/Header.jsx
@@ -6,28 +6,33 @@ export default function Header() {
     return(
         <header className="flex justify-center pt-[8px]">
                 <div className="flex justify-center pt-[96px] pb-[80px] bg-[#16073F] rounded-[18px]
-                max-xl:pt-[80px] max-xl:pb-[64px]">
+                max-xl:pt-[80px] max-xl:pb-[64px] max-lg:pt-[45px] max-lg:pb-[34px]">
                     <div className="flex flex-col items-start gap-[42px] 
-                    ps-[66px] max-2xl:gap-[36px] max-xl:gap-[30px]">
+                    ps-[66px] max-2xl:gap-[36px] max-xl:gap-[30px] max-lg:ps-[30px] max-md:gap-[22px]">
                         <h1 className="font-semibold text-[64px]
                         max-w-[747px] max-2xl:text-[48px] max-2xl:max-w-[585px]
-                        max-xl:text-[40px] max-xl:max-w-[487px]">Welcome to your ultimate destination for movie lovers!</h1>
+                        max-xl:text-[40px] max-xl:max-w-[487px] 
+                        max-lg:text-[30px] max-lg:max-w-[350px]
+                        max-md:text-[24px] max-md:max-w-[280px] 
+                        ">Welcome to your ultimate destination for movie lovers!</h1>
                         <h2 className="text-[32px] font-semibold
-                        max-2xl:text-[20px]
-                        max-xl:">All news in one place</h2>
+                        max-2xl:text-[20px] max-xl:text-[18px] max-md:text-[14px]">All news in one place</h2>
                         <Link to={'/movies'}>
                             <button className="flex gap-[18px] items-center font-semibold text-[20px] bg-[#FF2C78]
-                                p-[12px] rounded-[12px] max-2xl:text-[18px] max-xl:text-[16px]
+                                p-[12px] rounded-[12px] max-2xl:text-[18px] max-xl:text-[16px] max-lg:text-[14px]
+                                max-md:text-[12px] max-md:p-[10px] 
                                 transition-[150ms] hover:scale-[1.1]">Get Started 
                                 <img src={arr} alt="arrow"/>
                             </button>
                         </Link>
                     </div>
-                    <div className="flex gap-[15px] pe-[52px]">
+                    <div className="flex gap-[15px] pe-[52px] max-lg:pe-[30px]">
                         <img src={avengers} alt="" className="h-[440px] w-[310px]
-                        max-2xl:h-[340px] max-2xl:w-[210px] max-xl:h-[275px] max-xl:w-[180px]"/>
+                        max-2xl:h-[340px] max-2xl:w-[210px] max-xl:h-[235px] max-xl:w-[150px]
+                        max-lg:w-[135px] max-lg:h-[190px]"/>
                         <img src={fate} alt="" className="h-[440px] w-[310px] 
-                        max-2xl:h-[340px] max-2xl:w-[210px] max-xl:h-[275px] max-xl:w-[180px]"/>
+                        max-2xl:h-[340px] max-2xl:w-[210px] max-xl:h-[235px] max-xl:w-[150px]
+                        max-lg:w-[135px] max-lg:h-[190px]"/>
                     </div>
                 </div>
             </header>
--- MovieVerse/src/pages/Home/Main.jsx
+++ MovieVerse/src/pages/Home/Main.jsx
@@ -7,43 +7,44 @@ import tv from '../../assets/tv.png'
 
 export default function Main() {
     return(
-        <main className="flex flex-col items-center gap-[100px] pt-[100px] pb-[215px]">
-            <div className='flex items-center gap-[355px] max-2xl:gap-[255px] max-xl:gap-[155px]'>
+        <main className="flex flex-col items-center gap-[100px] max-lg:gap-[50px] pt-[100px] max-lg:pt-[50px] pb-[215px] max-lg:pb-[115px]">
+            <div className='flex items-center gap-[355px] max-2xl:gap-[255px] max-xl:gap-[155px]
+            max-lg:gap-[55px]'>
                 <img src={endgame} alt="endGame" className='w-[406px] h-[604px] max-2xl:w-[336px] max-2xl:h-[504px]
-                max-xl:w-[256px] max-xl:h-[404px]'/>
-                <div className="max-w-[482.5px] max-2xl:max-w-[420px]">
-                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px]">Save your favourite movie</h1>
-                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px]">Save feature and like feature are available.</h2>
+                max-xl:w-[256px] max-xl:h-[404px] max-lg:w-[200px] max-lg:h-[304px]'/>
+                <div className="max-w-[482.5px] max-2xl:max-w-[420px] max-lg:max-w-[320px]">
+                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[30px]">Save your favourite movie</h1>
+                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px] max-lg:text-[16px]">Save feature and like feature are available.</h2>
                 </div>
             </div>
             <div className='flex items-center gap-[150px] max-2xl:gap-[50px] max-xl:gap-[40px]'>
-                <div className="max-w-[547px] max-2xl:max-w-[485px] max-xl:max-w-[418px]">
-                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px]">Many rated movies under 18+ for children</h1>
-                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px]">List of movies rated under 18+ is available.</h2>
+                <div className="max-w-[547px] max-2xl:max-w-[485px] max-xl:max-w-[418px] max-lg:max-w-[290px]">
+                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[30px]">Many rated movies under 18+ for children</h1>
+                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px] max-lg:text-[16px]">List of movies rated under 18+ is available.</h2>
                 </div>
                 <img src={childMv} alt="childMovie" className='w-[546px] h-[415px] max-2xl:w-[446px] max-2xl:h-[335px]
-                max-xl:w-[346px] max-xl:h-[255px]'/>
+                max-xl:w-[346px] max-xl:h-[255px] max-lg:w-[245px] max-lg:h-[205px]'/>
             </div>
             <div className='flex items-center gap-[161px] max-2xl:gap-[111px] max-xl:gap-[60px]'>
                 <img src={tv} alt="tv" className='w-[610px] h-[514px] max-2xl:w-[510px] max-2xl:h-[414px]
-                max-xl:w-[410px] max-xl:h-[314px]'/>
+                max-xl:w-[410px] max-xl:h-[314px] max-lg:w-[210px] max-lg:h-[180px]'/>
                 <div className="max-w-[471.5px]">
-                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px]">Online movie list</h1>
-                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px]">Available on web application.</h2>
+                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[30px]">Online movie list</h1>
+                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px] max-lg:text-[16px]">Available on web application.</h2>
                 </div>
             </div>
-            <div className='flex items-center gap-[164px] max-2xl:gap-[136px] max-xl:gap-[35px]'>
-                <div className="max-w-[477px] max-2xl:max-w-[420px]">
-                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px]">Premium Feature</h1>
-                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px]">Register to access our save and like feature</h2>
+            <div className='flex items-center gap-[164px] max-2xl:gap-[136px] max-xl:gap-[35px] max-lg:gap-[60px]'>
+                <div className="max-w-[477px] max-2xl:max-w-[420px] max-lg:max-w-[330px]">
+                    <h1 className="font-semibold text-[48px] max-2xl:text-[40px] max-xl:text-[36px] max-lg:text-[30px]">Premium Feature</h1>
+                    <h2 className="text-[32px] max-2xl:text-[24px] max-xl:text-[20px] max-lg:text-[16px]">Register to access our save and like feature</h2>
                 </div>
-                <div className="flex gap-[27px]">
+                <div className="flex gap-[27px] max-lg:gap-[10px]">
                     <img src={profile1} alt="profile" className="w-[182px] h-[198px] max-2xl:w-[120px] max-2xl:h-[128px]
-                    max-xl:w-[100px] max-xl:h-[108px]"/>
+                    max-xl:w-[100px] max-xl:h-[108px] max-lg:w-[50px] max-lg:h-[58px]"/>
                     <img src={profile2} alt="profile" className="w-[182px] h-[198px] max-2xl:w-[120px] max-2xl:h-[128px]
-                    max-xl:w-[100px] max-xl:h-[108px]"/>
+                    max-xl:w-[100px] max-xl:h-[108px] max-lg:w-[50px] max-lg:h-[58px]"/>
                     <img src={profile3} alt="profile" className="w-[182px] h-[198px] max-2xl:w-[120px] max-2xl:h-[128px]
-                    max-xl:w-[100px] max-xl:h-[108px]"/>
+                    max-xl:w-[100px] max-xl:h-[108px] max-lg:w-[50px] max-lg:h-[58px]"/>
                 </div>
             </div>
         </main>
